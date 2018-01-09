7fb725b7ee1fa337fab1b5ce1ed2e516879874ba
anjarupnik <anjarupnik@hotmail.com>
2018-01-09 16:51:13 +0100

    multiple monsters

--- game.js
+++ game.js
@@ -1,17 +1,21 @@
+let points = 100
+
 function addMonsters() {
   const monster = $('<img src="http://res.cloudinary.com/mdfchucknorris/image/upload/v1515508131/Mikey_rlpj99.png" alt="monsters">')
   monster.css({
-    top: '-200px'
+    top: '-200px',
+    left: Math.floor(Math.random() * Math.floor(1000))
   })
 
-  $('.game').append(monster)
-  monster.animate({top: '100%'}, {
-    duration: 3000,
-    easing: 'linear',
-    complete: function() {
-      monster.remove()
-    }
-  })
+    $('.game').append(monster)
+    monster.animate({top: window.screen.height}, {
+      duration: 2000,
+      easing: 'linear',
+      complete: function() {
+        monster.remove()
+        addMonsters()
+      }
+    })
 }
 
 $(document).ready(function() {
